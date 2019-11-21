09a6834f545510c41f6319308eaac7fe2276d86e
wolfag <wolfag91@gmail.com>
2019-11-21 15:29:55 +0700

    scan qrcode

---
+++ App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import ScanScreen from './src/ScanScreen';
+
+const App = () => {
+  return <ScanScreen />;
+};
+
+export default App;
